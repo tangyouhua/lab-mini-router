2f5aa94f40b0d53cd4ea748935aa57a2a996edfb
Youhua Tang <tangyouhua@gmail.com>
2023-01-14 12:28:04 +0800

    搭建router项目骨架

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <img src="/vite.svg" class="logo" alt="Vite logo" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  <p>
+    <!-- 使用router-link导航 -->
+    <router-link to="/">Go to Home | </router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+
+  <!-- 路由出口 -->
+  <!-- 将来路由匹配的组件会渲染在这里 -->
+  <router-view></router-view>
 </template>
 
 <style scoped>
